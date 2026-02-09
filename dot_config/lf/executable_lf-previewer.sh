e73fa308b7f30efbfc10580cfe34a065b600dd81
kurrycat <44340227+kurrycat2004@users.noreply.github.com>
2026-02-09 15:48:19 +0100

    update lf config

---
+++ dot_config/lf/executable_lf-previewer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+case "$1" in
+    *.tar*) tar tf "$1";;
+    *.zip) unzip -l "$1";;
+    *.rar) unrar l "$1";;
+    *.7z) 7z l "$1";;
+    *.pdf) pdftotext "$1" -;;
+    *) bat --force-colorization --paging=never --style=numbers --terminal-width $(($2 - 5)) -f "$1" ;;
+esac
